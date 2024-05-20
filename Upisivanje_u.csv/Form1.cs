f9461ba0ef96f9fd1aa159adfa26c8b9e7664edf
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-05-20 21:44:40 +0200

    Add files via upload

---
+++ Upisivanje_u.csv/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Upisivanje_u.csv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string surname = textBox2.Text;
+            int yearOfBirth = (int)numericUpDown1.Value;
+            string email = textBox3.Text;
+
+            string[] lines = { $"{name},{surname},{yearOfBirth},{email}" };
+
+            File.AppendAllLines("user_data.csv", lines);
+
+            MessageBox.Show("Data saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
